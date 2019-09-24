12949aa5fe1001d1c875a24d262e9567eff3307b
Zii <dczii@live.com>
2019-09-24 17:13:33 +0800

    Updates

---
+++ precache-manifest.9231de179f3ef7373fffd559fe354307.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ba35796d9e4c9b5e75c9adf5757d38a",
+    "url": "/adzoft/index.html"
+  },
+  {
+    "revision": "99a07dd8c96370cd3326",
+    "url": "/adzoft/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "69d371e1af093a1cc22e",
+    "url": "/adzoft/static/js/2.d3f86d1d.chunk.js"
+  },
+  {
+    "revision": "99a07dd8c96370cd3326",
+    "url": "/adzoft/static/js/main.8f7bd39a.chunk.js"
+  },
+  {
+    "revision": "10c65295cc58743151e0",
+    "url": "/adzoft/static/js/runtime-main.56bfbbfe.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/adzoft/static/media/logo.25bf045c.svg"
+  }
+]);
