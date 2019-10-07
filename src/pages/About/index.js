854cecd0638727ac45243a3edf6b96f5a7a33695
Zii <dczii@live.com>
2019-10-07 14:05:35 +0800

    Fix mobile view.

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -13,25 +13,6 @@ function About(props) {
     return (
         <Grid container className='about-component' justify='center'>
             <Container>
-                <Grid item xs={12} className='center-container'>
-                    <div className='content'>
-                        <Typist avgTypingDelay={10} cursor={{show:false}}>
-                            <span>We help create your digital presence through carefully thought of</span>
-                        </Typist>
-                        <Typist avgTypingDelay={60} cursor={{show:false}}>
-                            <label>Designing</label>
-                        </Typist>
-                        <Typist avgTypingDelay={5} cursor={{show:false}}>
-                            <span>we turn that vision into a tangible, business-generating website through our expertise in</span>
-                        </Typist>
-                        <Typist avgTypingDelay={60} cursor={{show:false}}>
-                            <label>Development </label>
-                        </Typist>
-                        <Typist avgTypingDelay={30} cursor={{show:false}}>
-                            <span>& Custom Solutions</span>
-                        </Typist>
-                    </div>
-                </Grid>
 
                 <Grid item xs={12} className='display-center'>
                         <ScrollAnimation animateIn="fadeIn" animateOut="fadeOut">
@@ -77,6 +58,7 @@ function About(props) {
                         </div>
                     </Grid>
                 </Grid>
+
             </Container>
         </Grid>
     );
