1692f2a7c896e6fe874f593978f06525f152832d
Zii <dczii@live.com>
2019-10-29 20:10:09 +0800

    Fix contact page.

--- src/pages/ContactUs/index.js
+++ src/pages/ContactUs/index.js
@@ -1,20 +1,25 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
+import Icon from '@mdi/react'
+import { mdiRhombus } from '@mdi/js';
 
 import './styles.scss';
 
 function App(props) {
   return (
     <Grid container className='contact-component'>
+        <h1 style={{ marginBottom: 40 }}>
+            <Icon path={mdiRhombus} size={1} color='white' /> Contact Us
+        </h1>
         <a href="mailto:sales@adzoft.com">
-            <h1>
+            <h3>
                 sales@adzoft.com
-            </h1>
+            </h3>
         </a>
         <a href="#">
-            <h1>
+            <h3>
                 Facebook
-            </h1>
+            </h3>
         </a>
     </Grid>
   );
