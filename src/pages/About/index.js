6dfb948af5637389d0d52b7688c90d73ed58a122
Zii <dczii@live.com>
2019-11-07 16:15:30 +0800

    UI Adjustments.

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -14,47 +14,34 @@ function About(props) {
             <Container>
 
                 <Grid item xs={12} className='display-center'>
-                        <ScrollAnimation animateIn="fadeIn" animateOut="fadeOut">
-                            <h1><Icon path={mdiRhombus} size={1} color='white' /> Ready for the future <Icon path={mdiRhombus} size={1} color='white' /></h1>
-                        </ScrollAnimation>
-                        <ScrollAnimation animateIn="fadeInLeft" animateOut="fadeOutRight">
-                            <p>We are adzoft. We guide businesses in this rapidly changing digital world. We are a team of developers, entrepreneurs and idealists.We want to make an impact in our world by encouraging businesses to innovate and help them tap into an untouched market to open new opportunities</p>
-                        </ScrollAnimation>
+                    <h1 className="header1">Ready for the future</h1>
+                    <p className="p1">We are adzoft. We guide businesses in this rapidly changing digital world. We are a team of developers, entrepreneurs and idealists.We want to make an impact in our world by encouraging businesses to innovate and help them tap into an untouched market to open new opportunities</p>
                 </Grid>
                 
                 <Grid container className='block-2' justify='center' alignItems="stretch" spacing={2}>
 
                     <Grid item xs={12} sm={4} className='display-center'>
                         <div className='vision-container'>
-                            <ScrollAnimation animateIn="fadeIn" animateOut="fadeOut">
-                                <h1><Icon path={mdiRhombus} size={1} color='white' /> Our Mission </h1>
-                            </ScrollAnimation>
-                            <ScrollAnimation animateIn="fadeInLeft" animateOut="fadeOutRight">
-                                <p>Our mission is to help businesses and individuals to take advantage of today's technology to help them succeed and evolve to their full potential through digitization. Our team is passionate about delivering excellent services and experiences that will keep our clients pleased.</p>
-                            </ScrollAnimation>
+                            <h1 className="header2"> Our<br />Mission </h1>
+                            <p>Our mission is to help businesses and individuals to take advantage of today's technology to help them succeed and evolve to their full potential through digitization. Our team is passionate about delivering excellent services and experiences that will keep our clients pleased.</p>
                         </div>
+                        <div className='shadow'/>
                     </Grid>
                     
                     <Grid item xs={12} sm={4} className='display-center'>
                         <div className='vision-container'>
-                            <ScrollAnimation animateIn="fadeIn" animateOut="fadeOut">
-                                <h1><Icon path={mdiRhombus} size={1} color='white' /> Our Vision </h1>
-                            </ScrollAnimation>
-                            <ScrollAnimation animateIn="fadeInLeft" animateOut="fadeOutRight">
-                                <p>Our vision is to bring world-class design and development services to our clients and offer boundless opportunities to them by unlocking the doors to the digital world. We want to be the go-to company of businesses all over the world when they want to enter the digital world.</p>
-                            </ScrollAnimation>
+                            <h1 className="header2"> Our<br />Vision </h1>
+                            <p>Our vision is to bring world-class design and development services to our clients and offer boundless opportunities to them by unlocking the doors to the digital world. We want to be the go-to company of businesses all over the world when they want to enter the digital world.</p>
                         </div>
+                        <div className='shadow'/>
                     </Grid>
                     
                     <Grid item xs={12} sm={4} className='display-center'>
                         <div className='vision-container'>
-                            <ScrollAnimation animateIn="fadeIn" animateOut="fadeOut">
-                                <h1><Icon path={mdiRhombus} size={1} color='white' /> Our Impact </h1>
-                            </ScrollAnimation>
-                            <ScrollAnimation animateIn="fadeInLeft" animateOut="fadeOutRight">
-                                <p>Through our work, we are able to transform the landscape for businesses and individuals so they can stay up to date, ready to tackle the future and stay relevant in this fast changing world.</p>
-                            </ScrollAnimation>
-                        </div>
+                            <h1 className="header2"> Our<br />Impact </h1>
+                            <p>Through our work, we are able to transform the landscape for businesses and individuals so they can stay up to date, ready to tackle the future and stay relevant in this fast changing world.</p>
+                    </div>
+                    <div className='shadow'/>
                     </Grid>
                 </Grid>
 
