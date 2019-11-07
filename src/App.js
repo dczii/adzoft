5192804b21da1a5f7536a234f4bf4861fbb5823f
Zii <dczii@live.com>
2019-11-07 16:32:59 +0800

    Fix deploy error.

--- src/App.js
+++ src/App.js
@@ -4,8 +4,6 @@ import { BrowserRouter as Router } from "react-router-dom";
 import Header from './components/header';
 import Routes from './routes';
 
-import '../node_modules/react-typist/dist/standalone/Typist';
-
 function App({location}) {
   console.log(location)
   return (
