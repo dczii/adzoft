ca309f0b394167ab7230b2edb2d71fd22061120e
Zii <dczii@live.com>
2019-11-08 16:01:50 +0800

    Adjust bg colors.

--- src/components/button/index.js
+++ src/components/button/index.js
@@ -8,18 +8,18 @@ const CustomButton = withStyles({
     root: {
       fontSize: 16,
       padding: '6px 12px',
-      border: '3px solid #2096BA',
+      border: '3px solid #71c1b7',
       fontWeight: 700,
       letterSpacing: '0.2em',
       transition: 'color 0.1s ease 0.1s',
       lineHeight: 1.5,
       backgroundColor: '#FFF',
-      color: '#2096BA',
+      color: '#71c1b7',
       borderRadius: 19,
       
       '&:hover': {
-        backgroundColor: '#2096BA',
-        borderColor: '#2096BA',
+        backgroundColor: '#71c1b7',
+        borderColor: '#71c1b7',
         color: '#FFF'
       },
     },
