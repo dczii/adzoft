49302d3e5ed6686e54bc14f09b8dd7ec9167bf7c
Zii <dczii@live.com>
2019-11-08 13:58:27 +0800

    Rollback home view.

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useLayoutEffect } from 'react';
 import Grid from '@material-ui/core/Grid';
 import Container from '@material-ui/core/Container';
 import SwipeableViews from 'react-swipeable-views';
@@ -14,53 +14,97 @@ import './styles.scss';
 
 function Home(props) {
     const [activeIndex, setIndex] = useState(0);
+    const [width] = useWindowSize();
+
+
+    function useWindowSize() {
+        const [size, setSize] = useState([0, 0]);
+        useLayoutEffect(() => {
+          function updateSize() {
+            setSize([window.innerWidth]);
+          }
+          window.addEventListener('resize', updateSize);
+          updateSize();
+          return () => window.removeEventListener('resize', updateSize);
+        }, []);
+        return size;
+    }
+
+    const renderWork = (xs, md) => {
+        return <Grid item xs={xs} md={md} className='section-work' 
+            // onClick={() => props.history.push('/works')}
+        >
+            <img src={WorkSVG} className='work-svg' alt='SVG' />
+            <h2>We create beautiful and functional websites for individuals and businesses. {width}</h2>
+        </Grid>
+    }
+
+    const renderAbout = (xs, md) => {
+        return <Grid item xs={xs} md={md} className='section-vision' 
+            // onClick={() => props.history.push('/about')}
+        >
+            <img src={VisionSVG} className='vision-svg' alt='SVG' />
+            <Container className='content-container'>
+                <p className='title'>ABOUT</p>
+                <h3>Who we are and<br />Why we do<br />What we do.</h3>
+            </Container>
+        </Grid>
+    }
+
+    const renderContact = (xs, md) => {
+        return <Grid item xs={xs} md={md} className='section-vision section-team' 
+            // onClick={() => props.history.push('/contact-us')}
+        >
+            <img src={StudioSVG} className='contact-svg' alt='SVG' />
+            <Container className='content-container mobile-contact-container'>
+                <p className='title'>CONTACT US</p>
+                <h3>Interested in working with us?<br />Send us a message.</h3>
+            </Container>
+        </Grid>
+    }
+
+    const renderWebView = () => {
+        return <Grid container>
+            {renderWork(12, 8)}
+            <Grid item xs={4}>
+                <Grid container className='App mobile-block-2'>
+                    {renderAbout(12, 12)}
+                    {renderContact(12, 12)}
+                </Grid>
+            </Grid>
+        </Grid>
+
+    }
+
+    const renderMobileView = () => {
+        return <SwipeableViews style={{width: '100%', height: '100%'}}
+            enableMouseEvents
+            onChangeIndex={(index) => setIndex(index)}
+            index={activeIndex}
+        >
+            {renderWork(12,12)}
+            <Grid item xs={12}>
+                <Grid container className='App mobile-block-2'>
+                    {renderAbout(12, 4)}
+                    {renderContact(12, 4)}
+                    
+                </Grid>
+            </Grid>
+        </SwipeableViews>
+    }
 
     return (
         <Grid container className='home-component'>
-            <SwipeableViews style={{width: '100%', height: '100%'}}
-                enableMouseEvents
-                onChangeIndex={(index) => setIndex(index)}
-                index={activeIndex}
-            >
-                <Grid item xs={12} md={12} className='section-work' 
-                    // onClick={() => props.history.push('/works')}
-                >
-                    <img src={WorkSVG} className='work-svg' alt='SVG' />
-                    <h2>We create beautiful and functional websites for individuals and businesses.</h2>
-                </Grid>
-                <Grid item xs={12}>
-                    <Grid container className='App mobile-block-2'>
-                        <Grid item xs={12} md={6} className='section-vision' 
-                            // onClick={() => props.history.push('/about')}
-                        >
-                            <img src={VisionSVG} className='vision-svg' alt='SVG' />
-                            <Container className='content-container'>
-                                <p className='title'>ABOUT</p>
-                                <h3>Who we are and<br />Why we do<br />What we do.</h3>
-                            </Container>
-                        </Grid>
+            {width >= 768 ? renderWebView(): renderMobileView()}
 
-                        <Grid item xs={12} md={6} className='section-vision section-team' 
-                            // onClick={() => props.history.push('/contact-us')}
-                        >
-                            <img src={StudioSVG} className='contact-svg' alt='SVG' />
-                            <Container className='content-container mobile-contact-container'>
-                                <p className='title'>CONTACT US</p>
-                                <h3>Interested in working with us?<br />Send us a message.</h3>
-                            </Container>
-                        </Grid>
-                    </Grid>
-                </Grid>
-            </SwipeableViews>
-
-            <div className='btn-slider'>
+            {width <= 767 ? <div className='btn-slider'>
                 <Icon size={0.5} color='white' 
                     path={activeIndex === 0 ? mdiCheckboxBlankCircle : mdiCheckboxBlankCircleOutline}
                 />
                 <Icon size={0.5} color='white' 
                     path={activeIndex === 1 ? mdiCheckboxBlankCircle : mdiCheckboxBlankCircleOutline}
                 />
-            </div>
+            </div> : ''}
         </Grid>
     );
 }
